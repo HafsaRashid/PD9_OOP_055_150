c93fe07f93cb048f4d5ab2c9b693762559eebe80
HafsaRashid <81868979+HafsaRashid@users.noreply.github.com>
2021-04-25 05:51:05 +0500

    Updated task3

--- Task3/src/task3/Task3.java
+++ Task3/src/task3/Task3.java
@@ -4,11 +4,7 @@
  * and open the template in the editor.
  */
 package task3;
-
-/**
- *
- * @author Hafsa Rashid
- */
+import java.util.Scanner;
 public class Task3 {
 
     /**
@@ -16,7 +12,52 @@ public class Task3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("My name is hafsa");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter string with spaces: ");
+        String str = scanner.nextLine();
+        String ridSpace = ridMultipleBlank(str);
+        System.out.print("Enter string with integers: ");
+        String str2 = scanner.nextLine();
+        String ridNum = removeInteger(str2);
+        System.out.print("Enter string to encrypt: ");
+        String str3 = scanner.nextLine();
+        String Encrypt = stringEncryption(3);
+        System.out.println("Updated string is: " + ridSpace);
+        System.out.println("Updated string is: " + ridNum);
+        System.out.println("Encrypted string is: " + Encrypt);
+    }
+    public static String ridMultipleBlank(String s)
+    {
+        String str = "";
+        int length = s.length();
+        for(int i = 0; i < length; i++)
+        {
+            str = (s.replaceAll("( +)"," ").trim());
+                
+        }
+        return str;
     }
     
+    public static String removeInteger(String s) 
+    {
+        String str = "";
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) 
+        {
+            str = (s.replaceAll("[0-9]", ""));
+        }
+        return str;
+    }
+    
+    /*public static String stringEncryption(String s) 
+    {
+        String str = "";
+        int length = s.length();
+        for (int i = 0; i < length; i++) 
+        {
+            
+        }
+        return str;
+    }*/
 }
